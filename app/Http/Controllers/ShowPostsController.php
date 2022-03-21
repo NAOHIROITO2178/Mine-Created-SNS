1bda93addf50dedd4e03082059bc989c715b1176
NAOHIROITO2178 <korokoro78@ezweb.ne.jp>
2022-03-21 19:55:13 +0900

    コントローラー内のメゾッドの設定

--- app/Http/Controllers/ShowPostsController.php
+++ app/Http/Controllers/ShowPostsController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ShowPostsController extends Controller
 {
-    //
+    public function ShowPostsPage(){
+        return view(showposts);
+    }
+
+    public function postContents()
+    {
+        
+    }
 }
