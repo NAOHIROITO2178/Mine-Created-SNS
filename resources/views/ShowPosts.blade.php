ac2209521b286d6fd7162e562bb65101ff200d60
NAOHIROITO2178 <korokoro78@ezweb.ne.jp>
2022-03-24 19:39:42 +0900

    ログイン状態でのみ投稿できるように設定

--- resources/views/ShowPosts.blade.php
+++ resources/views/ShowPosts.blade.php
@@ -5,8 +5,15 @@
         {!! Form::open(['route' => 'showposts', 'method' => 'POST']) !!}
             {{ csrf_field() }}
             <div class="row mb-4">
+            @guest
+                <div class="mx-auto">
+                    <a class="btn btn-primary" href="{{ route('login') }}">ログインして投稿</a>
+                    <a class="btn btn-primary" href="{{ route('register') }}">新規登録して投稿</a>
+                </div>
+            @else
                 {{ Form::text('newPosts', null, ['class' => 'form-control col-9 mr-auto']) }}
                 {{ Form::submit('投稿する', ['class' => 'btn btn-primary col-2']) }}
+            @endguest
             </div>
         @if ($errors->has('post'))
             <p class="alert alert-danger">{{ $errors->first('post') }}</p>
